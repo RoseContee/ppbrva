4288caebddd08a3c81011a1c62610fff1e20f5fc
rosecontee <rosecontee@outlook.com>
2023-11-18 00:26:42 +0100

    2023-11-17

--- mobile/src/utils/theme.ts
+++ mobile/src/utils/theme.ts
@@ -65,7 +65,7 @@ const Theme = {
     shadowColor: Color.shadow,
     shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.3,
-    shadowRadius: 1,
+    shadowRadius: 1.5,
     elevation: 1.5,
   },
 };
