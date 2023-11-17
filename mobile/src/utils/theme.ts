e3f620d1f3da0f1cb69f226821b418eb80d5f7ea
rosecontee <rosecontee@outlook.com>
2023-11-18 00:04:49 +0100

    2023-11-17

--- mobile/src/utils/theme.ts
+++ mobile/src/utils/theme.ts
@@ -57,7 +57,7 @@ const Theme = {
   inputShadow: {
     shadowColor: Color.shadow,
     shadowOffset: {width: 0, height: 1},
-    shadowOpacity: 0.5,
+    shadowOpacity: 0,
     shadowRadius: BorderRadius.input,
     elevation: 1,
   },
